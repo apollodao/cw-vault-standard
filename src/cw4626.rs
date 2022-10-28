7b8c92c961837cdf3672d22ccaf313649a23e2d0
George McAskill <42020095+georgemc98@users.noreply.github.com>
2022-10-27 19:05:46 -0500

    feat: implement cw4626querymsg

--- src/cw4626.rs
+++ src/cw4626.rs
@@ -2,7 +2,7 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Binary, Empty, Uint128};
 use cw20::{Cw20Coin, Expiration, Logo};
 
-use crate::msg::ExtensionExecuteMsg;
+use crate::msg::{ExtensionExecuteMsg, ExtensionQueryMsg};
 
 #[cw_serde]
 pub enum Cw4626ExecuteMsg<T = ExtensionExecuteMsg, S = Empty> {
@@ -80,5 +80,57 @@ pub enum Cw4626ExecuteMsg<T = ExtensionExecuteMsg, S = Empty> {
 
     Callback(S),
 
+    VaultExtension(T),
+}
+
+#[cw_serde]
+pub enum Cw4626QueryMsg<T = ExtensionQueryMsg> {
+    //--------------------------------------------------------------------------------------------------
+    // Standard CW20 QueryMsgs
+    //--------------------------------------------------------------------------------------------------
+    /// Returns the current balance of the given address, 0 if unset.
+    /// Return type: BalanceResponse.
+    Balance {
+        address: String,
+    },
+    /// Returns metadata on the contract - name, decimals, supply, etc.
+    /// Return type: TokenInfoResponse.
+    TokenInfo {},
+    /// Only with "allowance" extension.
+    /// Returns how much spender can use from owner account, 0 if unset.
+    /// Return type: AllowanceResponse.
+    Allowance {
+        owner: String,
+        spender: String,
+    },
+    /// Only with "mintable" extension.
+    /// Returns who can mint and the hard cap on maximum tokens after minting.
+    /// Return type: MinterResponse.
+    Minter {},
+    /// Only with "marketing" extension
+    /// Returns more metadata on the contract to display in the client:
+    /// - description, logo, project url, etc.
+    /// Return type: MarketingInfoResponse.
+    MarketingInfo {},
+    /// Only with "marketing" extension
+    /// Downloads the embedded logo data (if stored on chain). Errors if no logo data stored for
+    /// this contract.
+    /// Return type: DownloadLogoResponse.
+    DownloadLogo {},
+    /// Only with "enumerable" extension (and "allowances")
+    /// Returns all allowances this owner has approved. Supports pagination.
+    /// Return type: AllAllowancesResponse.
+    AllAllowances {
+        owner: String,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+    /// Only with "enumerable" extension
+    /// Returns all accounts that have balances. Supports pagination.
+    /// Return type: AllAccountsResponse.
+    AllAccounts {
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
     VaultExtension(T),
 }
