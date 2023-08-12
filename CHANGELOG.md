0e5c8d554780ec0b70f864f628fa011a69ab6071
Pacman <pacman@apollo.farm>
2023-08-12 15:54:26 +0200

    fix: change type of version field to String
    Changes type of `VaultStandardInfoResponse::version` to String

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,3 +16,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Export `const VERSION`.
+
+## [0.4.0] - 2023-08-12
+
+### Changed
+
+- Changes type of `VaultStandardInfoResponse::version` from `u16` to `String`
--- src/msg.rs
+++ src/msg.rs
@@ -186,8 +186,9 @@ pub enum ExtensionQueryMsg {
 /// instead of needing to do a costly SmartQuery.
 #[cw_serde]
 pub struct VaultStandardInfoResponse {
-    /// The version of the vault standard used. A number, e.g. 1, 2, etc.
-    pub version: u16,
+    /// The version of the vault standard used by the vault as a semver compliant
+    /// string. E.g. "1.0.0" or "1.2.3-alpha.1"
+    pub version: String,
     /// A list of vault standard extensions used by the vault.
     /// E.g. ["lockup", "keeper"]
     pub extensions: Vec<String>,
