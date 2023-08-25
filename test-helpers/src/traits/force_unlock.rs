70678e213c5e3e9ffcc6a1aec4f248e592f42115
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-25 18:11:26 +0200

    fix: Use correct amount in redeem of test helper

--- test-helpers/src/traits/force_unlock.rs
+++ test-helpers/src/traits/force_unlock.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::Uint128;
+use cosmwasm_std::{coin, Coin, Uint128};
 use cw_it::test_tube::{Runner, SigningAccount};
 
 use cw_vault_standard::extensions::force_unlock::ForceUnlockExecuteMsg;
@@ -8,10 +8,11 @@ use cw_vault_standard::ExtensionExecuteMsg;
 use super::CwVaultStandardRobot;
 
 pub trait ForceUnlockVaultRobot<'a, R: Runner<'a> + 'a>: CwVaultStandardRobot<'a, R> {
-    fn force_redeem(
+    fn force_redeem_with_funds(
         &self,
         amount: impl Into<Uint128>,
         recipient: Option<String>,
+        funds: &[Coin],
         signer: &SigningAccount,
     ) -> &Self {
         self.wasm()
@@ -23,11 +24,26 @@ pub trait ForceUnlockVaultRobot<'a, R: Runner<'a> + 'a>: CwVaultStandardRobot<'a
                         amount: amount.into(),
                     },
                 )),
-                &[],
+                funds,
                 signer,
             )
             .unwrap();
         self
+    }
+
+    fn force_redeem(
+        &self,
+        amount: impl Into<Uint128>,
+        recipient: Option<String>,
+        signer: &SigningAccount,
+    ) -> &Self {
+        let amount: Uint128 = amount.into();
+        self.force_redeem_with_funds(
+            amount,
+            recipient,
+            &[coin(amount.u128(), self.vault_token())],
+            signer,
+        )
     }
 
     fn force_withdraw_unlocking(
